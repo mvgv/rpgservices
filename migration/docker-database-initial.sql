7629a4514153649caa3bc9b04b2890527063e374
mvgv <mvgv1989@gmail.com>
2022-07-31 16:55:10 -0300

    feat: definindo o esquema de dados inicial

---
+++ migration/docker-database-initial.sql
@@ -0,0 +1,11 @@
+create table personalidades(
+    id serial primary key,
+    nome varchar,
+    classe varchar
+);
+
+INSERT INTO personalidades(nome, classe) VALUES
+('Tragdush', 'Barbaro'),
+('Salazar', 'Mago'),
+('Faustus', 'Bruxo'),
+('Bedwyr', 'Guerreiro');
